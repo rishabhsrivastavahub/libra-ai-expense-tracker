02a48519f418ce13cc1275bcf89072c670ed5438
Rishabh Srivastava <rishabh.kumar.eca@gmail.com>
2026-06-07 12:30:04 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,27 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "http://localhost:5000/api",
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    "http://localhost:5000/api",
 });
 
 API.interceptors.request.use(
- (config) => {
-   const token = localStorage.getItem("token");
+  (config) => {
+    const token =
+      localStorage.getItem(
+        "token"
+      );
 
-   if (token) {
-     config.headers.Authorization = `Bearer ${token}`;
-   }
+    if (token) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
 
-   return config;
- },
- (error) => Promise.reject(error)
+    return config;
+  },
+  (error) =>
+    Promise.reject(error)
 );
 
 export default API;
